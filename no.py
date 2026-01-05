406b02cc2ebd5ce95c868133649a5f01eba8af24
My-school-account-ig <27olrees@sjhs.newport.sch.uk>
2026-01-05 13:56:38 +0000

    Update no.py
    Added house_hallway() subroutine

--- no.py
+++ no.py
@@ -13,6 +13,35 @@ time.sleep(1)
 
 inventory = []
 
+def house_hallway():
+    print("house_hallway() subroutine has been activated")
+    print("you went downstairs and entered the hallway")
+    print("""what do you want to do?
+1) go to the kitchen
+2) go outside
+3) go to the living room
+4) go to the dining room
+5) check inventory""")
+    choice = input(int(""))
+    if choice == "1":
+        warnings.warn("no subroutine", stacklevel=2)
+        sys.stderr.write("program has been terminated")
+    if choice =="2":
+        warnings.warn("no subroutine", stacklevel=2)
+        sys.stderr.write("program has been terminated")
+    if choice == "3":
+        print("You have died, please restart the game (there is no saving lol)")
+    if choice == "4":
+        print("You have died, please restart the game (there is no saving lol)")
+    if choice == "5":
+        warnings.warn("this will repeat the subroutine", stacklevel=2)
+        print("inventory: ", inventory)
+        house_hallway()
+    else:
+        warnings.warn("please input a valid integer", stacklevel=2)
+        warnings.warn("this will repeat the subroutine", stacklevel=2)
+        house_landing()
+
 def house_landing():
     print("house_landing() subroutine has been activated")
     print("you have entered the landing")
@@ -33,8 +62,7 @@ def house_landing():
     elif choice == "4":
         print("You have died, please restart the game (there is no saving lol)")
     elif choice == "5":
-        warnings.warn("no subroutine", stacklevel=2)
-        sys.stderr.write("program has been terminated")
+        house_hallway()
     elif choice == "6":
         warnings.warn("this will repeat the subroutine", stacklevel=2)
         print("inventory: ", inventory)
@@ -81,3 +109,4 @@ print("ono zombie caine aaaaaaa zombie aaaaaaaaaaaaaaaa - said co-caine")
 print("dgsafagrwigfrwuvfebvgiubjsbvv\bxzbv - says the zombie")
 print("ono the zombie has zombied the zombie and is going to zombie our zombie runnn! - said co-caine")
 house()
+
