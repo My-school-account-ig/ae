7276261216a8d05917b80397500b718222e9569d
My-school-account-ig <27olrees@sjhs.newport.sch.uk>
2026-02-12 15:01:07 +0000

    Update no.py

--- no.py
+++ no.py
@@ -4,27 +4,76 @@ import logging
 import warnings
 import random
 sys.stderr.write("err: import time is unused\n")
+sys.stderr.write("err: import logging is unused\n")
 sys.stderr.write("THERE IS AN ERROR ON LINE 55\n")
 sys.stderr.write("there is no number c on line 38\n")
 
-print("This part of the game is old and outdated, it won't be updated and debug mode is always on. soz! - the creator :3")
-time.sleep(1)
-print("If you want the updated version of the game, please download the latest version of the game on github (look at releases, it should be at the top, this will be at the bottom) - the (femboy) creator :3")
-time.sleep(1)
-
 x = int(random.randint(1, 3))
+
+knife = 0
 
 inventory = []
 
+def holyhead():
+    print("holyhead() subroutine has been activated")
+    choice = 0
+    print("you have arrived at holyhead and got some supplies")
+    sleep(5)
+    print("""what do you want to do?
+1. look for more supplies
+2. go back to llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch
+3. check inventory""")
+    choice = input(int())
+    if choice == "1":
+        warnings.warn("this will repeat the subroutine", stacklevel=2)
+        print("you look for more supplies")
+        holyhead()
+    elif choice == "2":
+        llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch()
+    elif choice == "3":
+        warnings.warn("this will repeat the subroutine", stacklevel=2)
+        print("inventory: ", inventory)
+        holyhead()
+    else:
+        warnings.warn("this will loop the subroutine", stacklevel=2)
+        warnings.warn("please input a valid integer", stacklevel=2)
+        holyhead()
+        
+def llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch():
+    print("llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch() subroutine has been activated")
+    choice = 0
+    print("you have reached llanfairpwllgwyngyllgogerychrywndrobwllllantysiliogogogoch after hours of driving")
+    sleep(5)
+    print("""what do you want to do?
+1) get some rest
+2) go get some more supplies from holyhead
+3) check inventory""")
+    choice = input(int())
+    if choice == "1":
+        print("you have completed the game")
+        print("theres nothing else to do now")
+    elif choice == "2":
+        warnings.warn("this subroutine doesn't exist", stacklevel=2)
+        sys.stderr.write("program has been terminated")
+    elif choice == "3":
+        warnings.warn("this will repeat the subroutine", stacklevel=2)
+        print("inventory: ", inventory)
+        llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch()
+    else:
+        warnings.warn("this will loop the subroutine", stacklevel=2)
+        warnings.warn("please input a valid integer", stacklevel=2)
+        llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch()
+
 def road_to_llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch():
     print("road_to_llanfairpwlldwyngyllgogerychwyrndrobwllllantysiliogogogoch() subroutine has been activated")
+    choice = 0
     print("holy thats a long subroutine name")
     print("you are on the road to llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch")
     print("""where do you want to go?
     1) keep going
     2) loot services
     3) check inventory""")
-    choice = input(int(""))
+    choice = input(int())
     if choice == "1":
         warnings.warn("this subroutine doesn't exist", stacklevel=2)
         sys.stderr.write("program has been terminated")
@@ -35,20 +84,22 @@ def road_to_llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch():
     elif choice == "3":
         warnings.warn("this will loop the subroutine", stacklevel=2)
         print("inventory: ", inventory)
-        road_to_llanfairpwllgwyngyllgogerychrwyndrobwllllantysiliogogogoch()
+        road_to_llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch()
     else:
         warnings.warn("this will loop the subroutine", stacklevel=2)
         warnings.warn("please input a valid integer", stacklevel=2)
-        road_to_llanfairpwllgwyngyllgogerychrwyndrobwllllantysiliogogogoch()
+        road_to_llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch()
 
 def m4_b():
     print("m4_b() subroutine has been activated")
+    choice = 0
     print("a")
+    sleep(5)
     print("""where do you want to go?
     1) back to newport
     2) go to llanfairpwllgwyngyllgogerychwryndrobwllllantysiliogogogoch
     3) check inventory""")
-    choice = input(int(""))
+    choice = input(int())
     if choice == "1":
         newport()
     elif choice == "2":
@@ -64,12 +115,14 @@ def m4_b():
 
 def cardiff():
     print("cardiff() subroutine has been activated")
+    choice = 0
     print("you have arrived at cardiff")
+    sleep(5)
     print("""what do you want to do?
     1) get supplies
     2) go back on the m4
     3) check inventory""")
-    choice = input(int(""))
+    choice = input(int())
     if choice == "1":
         warnings.warn("this will loop the subroutine", stacklevel=2)
         print("you got some supplies")
@@ -82,17 +135,19 @@ def cardiff():
         cardiff()
     else:
         warnings.warn("this will loop the subroutine", stacklevel=2)
-        warnigns.warn("please input a valid integer", stacklevel=2)
+        warnings.warn("please input a valid integer", stacklevel=2)
         cardiff()
 
 def m4_a():
     print("m4_a() subroutine has been activated")
+    choice = 0
     print("you are on the m4")
+    sleep(5)
     print("""where do you want to go?
 1) go to cardiff
 2) go to Llanfairpwllgwyngyllgogerychrwyndrobwllllantysiliogogoch
 3) check inventory""")
-    choice = input(int(""))
+    choice = input(int())
     if choice == "1":
         cardiff()
     elif choice == "2":
@@ -108,12 +163,14 @@ def m4_a():
 
 def newport():
     print("newport() subroutine has been activated")
+    choice = 0
     print("you can skip this if you want lol")
+    sleep(5)
     print("""what do you want to do?
 1) go get supplies
 2) go on the M4
 3) check inventory""")
-    choice = input(int(""))
+    choice = input(int())
     if choice == "1":
         print("you go get supplies and got food")
         warnings.warn("this will loop the subroutine", stacklevel=2)
@@ -131,11 +188,12 @@ def newport():
 
 def street():
     print("street() subroutine has been activated")
+    choice = 0
     print("you are now outside")
     print("""what do you want to do?
 1) get in the car
 2) check inventory""")
-    choice = input(int(""))
+    choice = input(int())
     if choice == "1":
         newport()
     elif choice =="2":
@@ -149,20 +207,22 @@ def street():
 
 def house_kitchen_cupboards():
     print("house_kitchen_cupboards() subroutine has been activated")
+    choice = 0
     print("you go to check the cupboards")
+    sleep(5)
     print("""check cupboard:
 1) cupboard 1
 2) cupboard 2
 3) cupboard 3
 4) go back
 5) check inventory""")
-    choice = input(int(""))
+    choice = input(int())
     if choice == "1":
         print("you check the cupboard")
         if x == "1":
             print("you found a knife")
             inventory.append("knife")
-            knife = True
+            knife = 1
             warnings.warn("This will loop the subroutine", stacklevel=2)
             house_kitchen_cupboards()
         else:
@@ -174,7 +234,7 @@ def house_kitchen_cupboards():
         if x == "2":
             print("you found a knife")
             inventory.append("knife")
-            knife = True
+            knife = 1
             warnings.warn("This will loop the subroutine", stacklevel=2)
             house_kitchen_cupboards()
         else:
@@ -186,7 +246,7 @@ def house_kitchen_cupboards():
         if x == "3":
             print("you found a knife")
             inventory.append("knife")
-            knife = True
+            knife = 1
             warnings.warn("This will loop the subroutine", stacklevel=2)
             house_kitchen_cupboards()
         else:
@@ -206,15 +266,17 @@ def house_kitchen_cupboards():
 
 def house_kitchen():
     print("house_kitchen() subroutine has been activated")
+    choice = 0
     print("you went into the kitchen")
+    sleep(5)
     print("""what do you want to do?
 1) fight zombie
 2) look in the cupboards
 3) check inventory""")
-    choice = input(int(""))
+    choice = input(int())
     if choice == "1":
         print("you go to fight the zombie")
-        if knife == True:
+        if knife == 1:
             print("you fight the zombie")
             print("you win")
             house_hallway()
@@ -235,14 +297,16 @@ def house_kitchen():
 
 def house_hallway():
     print("house_hallway() subroutine has been activated")
+    choice = 0
     print("you entered the hallway")
+    sleep(1)
     print("""what do you want to do?
 1) go to the kitchen
 2) go outside
 3) go to the living room
 4) go to the dining room
 5) check inventory""")
-    choice = input(int(""))
+    choice = input(int())
     if choice == "1":
         house_kitchen()
     if choice =="2":
@@ -262,7 +326,9 @@ def house_hallway():
 
 def house_landing():
     print("house_landing() subroutine has been activated")
+    choice = 0
     print("you have entered the landing")
+    sleep(5)
     print("""only 1 of 4 rooms will help you escape
 1) turn back
 2) bedroom 2
@@ -270,7 +336,7 @@ def house_landing():
 4) bathroom
 5) go downstairs
 6) inventory (does not progress the game)""")
-    choice = input(int(""))
+    choice = input(int())
     if choice == "1":
         print("You have died, please restart the game (there is no saving lol)")
     elif choice == "2":
@@ -293,6 +359,7 @@ def house_landing():
 def house_bedroom():
     print("house_bedroom() subroutine is activated")
     print("You need to leave and escape.")
+    sleep(5)
     print("""What do you want to do?
 1) leave the bedroom
 2) grab your phone
@@ -316,16 +383,20 @@ def house_bedroom():
 def house():
     print("house() subroutine is activated")
     print("this is the house where your're lives")
+    sleep(5)
     print("tiem to leave teh hose :3")
     house_bedroom()
 
 name = print("What's your're name?")
+sleep(5)
 sys.stderr.write("err: unused variable name\n")
 print("sike your're name is caine >:3")
+sleep(1)
 print(";w;")
+sleep(5)
 print("ono zombie caine aaaaaaa zombie aaaaaaaaaaaaaaaa - said co-caine")
+sleep(1)
 print("dgsafagrwigfrwuvfebvgiubjsbvv\bxzbv - says the zombie")
+sleep(1)
 print("ono the zombie has zombied the zombie and is going to zombie our zombie runnn! - said co-caine")
 house()
-
-
