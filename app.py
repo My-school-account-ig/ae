9b604ed63c7c4bf8cba1621a2a981ebcab20d517
My-school-account-ig <27olrees@sjhs.newport.sch.uk>
2026-03-23 13:46:39 +0000

    Update app.py
    Changed minwidth and maxwidth to minsize and maxsize.
    
    Github recognises the Canvas(root) function but Thonny (Python IDE my school uses) does not so I'll test this on my computer on VSCode to see it works.
    
    If it doesn't I'll see if the Canvas(root) function requires the tkinter.tkk import.

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ root = tk.Tk()
 
 root.title("storm chasing  gaem thinf :3")
 root.configure(background="black")
-root.minwidth(500, 500)
-root.maxwidth(1920, 1080)
+root.minsize(500, 500)
+root.maxsize(1920, 1080)
 
 canvas = Canvas(root)
 canvas.create_polygon(10, 10, 200, 50, 90, 150, 50, 80, 120, 55, outline="blue", width=2)
